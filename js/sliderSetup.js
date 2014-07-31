793c21f27fc335557d27b2e7548063739d0c155e
Frank Stepanski <frank@javascriptworkshop.com>
2014-07-31 16:08:54 -0400

    Initial commit

---
+++ js/sliderSetup.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){	
+			
+			
+			$('#layerslider').layerSlider({
+					skinsPath : 'slider/skin/',
+					globalBGColor : '#271505',
+					yourLogo			: 'assets/img/aslogo.png',
+					yourLogoStyle		: 'position: absolute; z-index: 1001; left: 0px; top: -94px; height: 265px;'
+				   
+
+				});
+				
+});	
